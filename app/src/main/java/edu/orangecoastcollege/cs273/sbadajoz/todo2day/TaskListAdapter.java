7704f3c834c7ab0a32ba78be6b9141720f6f8248
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-03 15:52:13 -0700

    Complete TaskListAdapter.java

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/todo2day/TaskListAdapter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/todo2day/TaskListAdapter.java
@@ -1,13 +1,44 @@
 package edu.orangecoastcollege.cs273.sbadajoz.todo2day;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+
+import java.util.List;
 
 
 public class TaskListAdapter extends ArrayAdapter<Task> {
-    public TaskListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull Task[] objects) {
+    private Context mContext;
+    private int mResourceID;
+    private List<Task> mTaskList;
+
+    public TaskListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Task> objects) {
         super(context, resource, objects);
+        mContext = context;
+        mResourceID = resource;
+        mTaskList = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Task selectedTask = mTaskList.get(position);
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(mResourceID, null);
+
+        CheckBox selectedCheckBox = (CheckBox) view.findViewById(R.id.isDoneCheckBox);
+
+        selectedCheckBox.setChecked(selectedTask.isDone());
+        selectedCheckBox.setText(selectedTask.getDescription());
+
+        return view;
     }
 }
