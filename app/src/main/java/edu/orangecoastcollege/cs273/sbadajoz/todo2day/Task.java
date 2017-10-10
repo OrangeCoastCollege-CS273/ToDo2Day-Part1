7f6d0efe06bb1aabd7d1d9905d182ca8e57b4554
Severiano Badajoz <sevebadajoz@gmail.com>
2017-10-10 13:36:57 -0700

    Create Documentation

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/todo2day/Task.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/todo2day/Task.java
@@ -1,7 +1,9 @@
 package edu.orangecoastcollege.cs273.sbadajoz.todo2day;
 
 /**
- * Created by sbadajoz on 9/28/2017.
+ * Represents a task
+ * Can be completed or not based on data
+ * Has a description that explains what needs to be done
  */
 
 public class Task {
@@ -9,37 +11,74 @@ public class Task {
     private String mDescription;
     private boolean mIsDone;
 
+    /**
+     * Constructor to be used by database fetches
+     *
+     * @param id ID of task in database
+     * @param description Description of task
+     * @param isDone Whether the task has been completed or not
+     */
     public Task(int id, String description, boolean isDone) {
         mID = id;
         mDescription = description;
         mIsDone = isDone;
     }
 
+    /**
+     * Constructor to be used by user data input
+     *
+     * @param description Description of task
+     * @param isDone Whether the task has been completed or not
+     */
     public Task(String description, boolean isDone) {
         mDescription = description;
         mIsDone = isDone;
     }
 
+    /**
+     * Returns the id of the task
+     * @return task ID
+     */
     public int getID() {
         return mID;
     }
 
+    /**
+     * Returns the description of the task
+     * @return task description
+     */
     public String getDescription() {
         return mDescription;
     }
 
+    /**
+     * Resets the description of the task
+     * @param description New task description
+     */
     public void setDescription(String description) {
         mDescription = description;
     }
 
+    /**
+     * Returns if the task has been completed or not
+     * @return True if task has been completed
+     */
     public boolean isDone() {
         return mIsDone;
     }
 
+    /**
+     * Sets if the task has been completed
+     * @param done True if task has been completed
+     */
     public void setDone(boolean done) {
         mIsDone = done;
     }
 
+    /**
+     * Basic string output of task data members
+     * @return A string of all the data regarding the task
+     */
     @Override
     public String toString() {
         return "Task{" +
